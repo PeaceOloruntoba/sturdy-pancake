7d76008f28d95a33d35d4711003642d3904563dd
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-07-02 11:21:33 +0100

    Create api.ts

---
+++ src/utils/api.ts
@@ -0,0 +1,32 @@
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
+import { useAuthStore } from "../store/useAuthStore";
+
+const api: AxiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use(
+  (config: AxiosRequestConfig) => {
+    const token = useAuthStore.getState().token;
+    if (token) {
+      config.headers = config.headers || {};
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+api.interceptors.response.use(
+  (response: AxiosResponse) => response,
+  (error) => {
+    const message =
+      error.response?.data?.error?.message || "An unexpected error occurred";
+    return Promise.reject({ message, status: error.response?.status || 500 });
+  }
+);
+
+export default api;
