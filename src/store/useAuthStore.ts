7d0776fb160904c181ec2fe193bb0c3c0edb8b45
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-07-02 11:38:56 +0100

    Update useAuthStore.ts

--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
+import { persist, createJSONStorage } from "zustand/middleware";
 import api from "../utils/api";
 
 interface User {
@@ -78,7 +78,7 @@ export const useAuthStore = create<AuthState>()(
     }),
     {
       name: "auth-storage",
-      storage: window.localStorage,
+      storage: createJSONStorage(() => localStorage),
     }
   )
 );
