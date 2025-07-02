3713797a88c1886fa8ca8f43286ad26f1f3c1c2a
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-07-02 15:44:43 +0100

    Update MainLayout.tsx

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -4,13 +4,15 @@ import Sidebar from "../components/Sidebar";
 
 export default function MainLayout() {
   return (
-    <div className="w-screen h-screen flex gap-4">
-      <div className="w-1/4">
+    <div className="w-screen h-screen flex flex-col md:flex-row bg-gray-100">
+      <div className="w-full md:w-1/4">
         <Sidebar />
       </div>
-      <div className="flex flex-col w-4/5">
+      <div className="flex flex-col w-full md:w-3/4 h-full">
         <Headbar />
-        <Outlet />
+        <div className="flex-1 overflow-y-auto p-4 animate-fadeIn">
+          <Outlet />
+        </div>
       </div>
     </div>
   );
