2a41ce1dce2f0e52ed404f9ba15813c8544329ad
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-07-02 11:29:57 +0100

    Update Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,5 @@
+import type { ReactNode } from "react";
 import { cn } from "../utils/cn";
-import { ReactNode } from "react";
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: "default" | "ghost" | "outline";
