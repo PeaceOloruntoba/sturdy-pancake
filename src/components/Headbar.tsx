a5bc049e3fb7588328fbee8b0a0334282b66e0d0
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-07-05 13:22:16 +0100

    Update Headbar.tsx

--- src/components/Headbar.tsx
+++ src/components/Headbar.tsx
@@ -1,5 +1,6 @@
 import { useLocation } from "react-router";
 import { FaHeart } from "react-icons/fa";
+import logo from "../assets/logo.JPG";
 
 export default function Headbar() {
   const location = useLocation();
@@ -11,7 +12,7 @@ export default function Headbar() {
   return (
     <div className="flex items-center justify-between p-4 bg-gradient-to-r from-rose-600 to-pink-600 text-white shadow-lg">
       <div className="flex items-center space-x-2 animate-fadeIn">
-        <FaHeart className="h-6 w-6 text-white" />
+        <img src={logo} alt="" className="w-12 h-12" />
         <span className="text-lg font-bold">
           Unistudents Match / {pageName}
         </span>
