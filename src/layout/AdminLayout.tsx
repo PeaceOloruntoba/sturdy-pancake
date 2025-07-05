8b0e4eb4964e2f01fe28dd57f3f8891893bf8ae5
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-07-05 10:22:09 +0100

    Create AdminLayout.tsx

---
+++ src/layout/AdminLayout.tsx
@@ -0,0 +1,19 @@
+import { Outlet } from "react-router";
+import Headbar from "../components/Headbar";
+import Sidebar from "../components/Sidebar";
+
+export default function MainLayout() {
+  return (
+    <div className="w-screen h-screen flex flex-col md:flex-row bg-gray-100">
+      <div className="w-full md:w-1/4">
+        <Sidebar />
+      </div>
+      <div className="flex flex-col w-full md:w-3/4 h-full">
+        <Headbar />
+        <div className="flex-1 overflow-y-auto p-4 animate-fadeIn">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
+}
