a5490fb8cfe81594add594b7fa9e095b2a8474d0
PeaceOloruntoba <profprincepeace@gmail.com>
2025-07-14 12:10:11 +0100

    ok

--- src/main.tsx
+++ src/main.tsx
@@ -3,20 +3,23 @@ import { createRoot } from "react-dom/client";
 import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 import "./index.css";
 import App from "./App.tsx";
+import { Elements } from "@stripe/react-stripe-js";
 
 const id = import.meta.env.VITE_PAYPAL_CLIENT_ID;
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <PayPalScriptProvider
-      options={{
-        clientId: id,
-        components: "buttons,card-fields",
-        intent: "authorize",
-        vault: true,
-      }}
-    >
-      <App />
-    </PayPalScriptProvider>
+    <Elements stripe={null}>
+      <PayPalScriptProvider
+        options={{
+          clientId: id,
+          components: "buttons,card-fields",
+          intent: "authorize",
+          vault: true,
+        }}
+      >
+        <App />
+      </PayPalScriptProvider>
+    </Elements>
   </StrictMode>
 );
