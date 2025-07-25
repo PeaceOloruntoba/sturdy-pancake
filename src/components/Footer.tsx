e223daafd1ced74bbc9bc94f0cc3691e9ea9826d
PeaceOloruntoba <profprincepeace@gmail.com>
2025-07-25 19:40:04 +0100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,12 @@
 import { FaHeart } from "react-icons/fa";
+import { logo } from "../assets";
 
 export default function Footer() {
   return (
     <footer className="bg-gray-900 text-white py-8 px-4">
       <div className="container mx-auto text-center">
         <div className="flex items-center justify-center space-x-2 mb-4">
-          <FaHeart className="h-6 w-6 text-rose-400" />
+                <img src={logo} alt="" className="w-12 h-12" />
           <span className="text-xl font-bold">Unistudents Match</span>
         </div>
         <p className="text-gray-400">
