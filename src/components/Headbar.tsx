c14ad3ecf11d9573992a3abe850e9f5b5e88a6ff
PeaceOloruntoba <profprincepeace@gmail.com>
2025-07-27 14:45:12 +0100

    Update Headbar.tsx

--- src/components/Headbar.tsx
+++ src/components/Headbar.tsx
@@ -11,7 +11,7 @@ export default function Headbar() {
   return (
     <div className="flex items-center justify-between p-4 bg-gradient-to-r from-rose-600 to-pink-600 text-white shadow-lg">
       <div className="flex items-center space-x-2 animate-fadeIn">
-        <img src={logo} alt="" className="w-12 h-12" />
+        <img src={logo} alt="" className="w-18 h-14" />
         <span className="text-lg font-bold">
           Unistudents Match / {pageName}
         </span>
